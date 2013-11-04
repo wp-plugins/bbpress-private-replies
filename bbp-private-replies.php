2a1bc0328b16714e9ac6d45fa3e51349774ee812
mordauk <mordauk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-04 15:25:00 +0000

    Uploading v1.0.11
    git-svn-id: https://plugins.svn.wordpress.org/bbpress-private-replies/tags/1.0.11@798637 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bbp-private-replies.php
@@ -0,0 +1,253 @@
+<?php
+/*
+Plugin Name: bbPress - Private Replies
+Plugin URL: http://pippinsplugins.com/bbpress-private-replies
+Description: Allows users to set replies as private so that only the original poster and admins can see it
+Version: 1.0.11
+Author: Pippin Williamson and Remi Corson
+Author URI: http://pippinsplugins.com
+Contributors: mordauk, corsonr
+Text Domain: bbp_private_replies
+Domain Path: languages
+*/
+
+class BBP_Private_Replies {
+
+	/*--------------------------------------------*
+	 * Constructor
+	 *--------------------------------------------*/
+
+	/**
+	 * Initializes the plugin by setting localization, filters, and administration functions.
+	 */
+	function __construct() {
+
+		// load the plugin translation files
+		add_action( 'init', array( $this, 'textdomain' ) );
+
+		// show the "Private Reply?" checkbox
+		add_action( 'bbp_theme_before_reply_form_submit_wrapper', array( $this, 'checkbox' ) );
+
+		// save the private reply state
+		add_action( 'bbp_new_reply',  array( $this, 'update_reply' ), 0, 6 );
+		add_action( 'bbp_edit_reply',  array( $this, 'update_reply' ), 0, 6 );
+
+		// hide reply content
+		add_filter( 'bbp_get_reply_excerpt', array( $this, 'hide_reply' ), 999, 2 );
+		add_filter( 'bbp_get_reply_content', array( $this, 'hide_reply' ), 999, 2 );
+		add_filter( 'the_content', array( $this, 'hide_reply' ), 999 );
+		add_filter( 'the_excerpt', array( $this, 'hide_reply' ), 999 );
+
+		// prevent private replies from being sent in email subscriptions
+		add_filter( 'bbp_subscription_mail_message', array( $this, 'prevent_subscription_email' ), 10, 4 );
+
+		// add a class name indicating the read status
+		add_filter( 'post_class', array( $this, 'reply_post_class' ) );
+
+		// register css files
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles' ) );
+
+	} // end constructor
+
+
+	/**
+	 * Load the plugin's text domain
+	 *
+	 * @since 1.0
+	 *
+	 * @return void
+	 */
+	public function textdomain() {
+		load_plugin_textdomain( 'bbp_private_replies', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+
+
+	/**
+	 * Outputs the "Set as private reply" checkbox
+	 *
+	 * @since 1.0
+	 *
+	 * @return void
+	 */
+	public function checkbox() {
+
+?>
+		<p>
+
+			<input name="bbp_private_reply" id="bbp_private_reply" type="checkbox"<?php checked( '1', $this->is_private( bbp_get_reply_id() ) ); ?> value="1" tabindex="<?php bbp_tab_index(); ?>" />
+
+			<?php if ( bbp_is_reply_edit() && ( get_the_author_meta( 'ID' ) != bbp_get_current_user_id() ) ) : ?>
+
+				<label for="bbp_private_reply"><?php _e( 'Set author\'s post as private.', 'bbp_private_replies' ); ?></label>
+
+			<?php else : ?>
+
+				<label for="bbp_private_reply"><?php _e( 'Set as private reply', 'bbp_private_replies' ); ?></label>
+
+			<?php endif; ?>
+
+		</p>
+<?php
+
+	}
+
+
+	/**
+	 * Stores the private state on reply creation and edit
+	 *
+	 * @since 1.0
+	 *
+	 * @param $reply_id int The ID of the reply
+	 * @param $topic_id int The ID of the topic the reply belongs to
+	 * @param $forum_id int The ID of the forum the topic belongs to
+	 * @param $anonymous_data bool Are we posting as an anonymous user?
+	 * @param $author_id int The ID of user creating the reply, or the ID of the replie's author during edit
+	 * @param $is_edit bool Are we editing a reply?
+	 *
+	 * @return void
+	 */
+	public function update_reply( $reply_id = 0, $topic_id = 0, $forum_id = 0, $anonymous_data = false, $author_id = 0, $is_edit = false ) {
+
+		if( isset( $_POST['bbp_private_reply'] ) )
+			update_post_meta( $reply_id, '_bbp_reply_is_private', '1' );
+		else
+			delete_post_meta( $reply_id, '_bbp_reply_is_private' );
+
+	}
+
+
+	/**
+	 * Determines if a reply is marked as private
+	 *
+	 * @since 1.0
+	 *
+	 * @param $reply_id int The ID of the reply
+	 *
+	 * @return bool
+	 */
+	public function is_private( $reply_id = 0 ) {
+
+		$retval 	= false;
+
+		// Checking a specific reply id
+		if ( !empty( $reply_id ) ) {
+			$reply     = bbp_get_reply( $reply_id );
+			$reply_id = !empty( $reply ) ? $reply->ID : 0;
+
+		// Using the global reply id
+		} elseif ( bbp_get_reply_id() ) {
+			$reply_id = bbp_get_reply_id();
+
+		// Use the current post id
+		} elseif ( !bbp_get_reply_id() ) {
+			$reply_id = get_the_ID();
+		}
+
+		if ( ! empty( $reply_id ) ) {
+			$retval = get_post_meta( $reply_id, '_bbp_reply_is_private', true );
+		}
+
+		return (bool) apply_filters( 'bbp_reply_is_private', (bool) $retval, $reply_id );
+	}
+
+
+	/**
+	 * Hides the reply content for users that do not have permission to view it
+	 *
+	 * @since 1.0
+	 *
+	 * @param $content string The content of the reply
+	 * @param $reply_id int The ID of the reply
+	 *
+	 * @return string
+	 */
+	public function hide_reply( $content = '', $reply_id = 0 ) {
+
+		if( empty( $reply_id ) )
+			$reply_id = get_the_ID();
+
+		if( $this->is_private( $reply_id ) ) {
+
+			$current_user = wp_get_current_user();
+
+			$topic_author = bbp_get_topic_author_id();
+			$reply_author = bbp_get_reply_author_id( $reply_id );
+
+			if( ( ( $topic_author != $current_user->ID && $topic_author != $reply_author ) && $reply_author != $current_user->ID && ! current_user_can( 'moderate' ) ) || ! is_user_logged_in() ) {
+
+				$content = __( 'This reply has been marked as private.', 'bbp_private_replies' );
+
+			}
+
+		}
+
+		return $content;
+	}
+
+
+	/**
+	 * Prevents a New Reply notification from being sent if the user doesn't have permission to view it
+	 *
+	 * @since 1.0
+	 *
+	 * @param $message string The email message
+	 * @param $reply_id int The ID of the reply
+	 * @param $topic_id int The ID of the reply's topic
+	 * @param $user_id int The ID of the user receiving the notification
+	 *
+	 * @return mixed
+	 */
+	public function prevent_subscription_email( $message, $reply_id, $topic_id, $user_id ) {
+
+		if( ! $this->is_private( $reply_id ) )
+			return $message; // reply isn't private so return message unchanged
+
+		$topic_author = bbp_get_topic_author_id( $topic_id );
+		$reply_author = bbp_get_reply_author_id( $reply_id );
+
+		if( ( $topic_author != $user_id || $topic_author != $reply_author ) && $reply_author != $user_id && ! user_can( $user_id, 'moderate' ) )
+			return false; // this prevents the email from getting sent
+
+		return $message; // message unchanged
+	}
+
+
+	/**
+	 * Adds a new class to replies that are marked as private
+	 *
+	 * @since 1.0
+	 *
+	 * @param $classes array An array of current class names
+	 *
+	 * @return bool
+	 */
+	public function reply_post_class( $classes ) {
+
+		$reply_id = bbp_get_reply_id();
+
+		// only apply the class to replies
+		if( bbp_get_reply_post_type() != get_post_type( $reply_id ) )
+			return $classes;
+
+		if( $this->is_private( $reply_id ) )
+			$classes[] = 'bbp-private-reply';
+
+		return $classes;
+	}
+
+	/**
+	 * Load the plugin's CSS files
+	 *
+	 * @since 1.0
+	 *
+	 * @return void
+	 */
+	public function register_plugin_styles() {
+		$css_path = plugin_dir_path( __FILE__ ) . 'css/frond-end.css';
+	    wp_enqueue_style( 'bbp_private_replies_style', plugin_dir_url( __FILE__ ) . 'css/frond-end.css', filemtime( $css_path ) );
+	}
+
+} // end class
+
+// instantiate our plugin's class
+$GLOBALS['bbp_private_replies'] = new BBP_Private_Replies();
---
+++ readme.txt
@@ -0,0 +1,102 @@
+=== bbPress - Private Replies ===
+Author URI: http://pippinsplugins.com
+Plugin URI: http://pippinsplugins.com/bbpress-private-replies
+Contributors: mordauk, corsonr
+Donate link: http://pippinsplugins.com/support-the-site
+Tags: bbPress, private replies, replies, Forums, mordauk, Pippin Williamson, pippinsplugins, Remi Corson, corsonr
+Requires at least: 3.2
+Tested up to: 3.5
+Stable Tag: 1.0.11
+
+
+A simple plugin to allow your bbPress users to mark their replies as private.
+
+== Description ==
+
+This add-on plugin for bbPress will allow your forum posters to mark their replies as private, meaning that only the original poster and forum moderators can see the content of the reply. This is a great plugin to install if you use bbPress as a support forum where users may need to share confidential information, such as site URLs, passwords, etc.
+
+If you have suggestions or bugfixes for the plugin, please report them on [Github](https://github.com/pippinsplugins/bbPress-Private-replies).
+
+**Languages**
+
+Private Replies for bbPress as been translated into the following languages:
+
+1. English
+2. French
+
+Would you like to help translate the plugin into more languages? [Contact Pippin](http://pippinsplugins.com/contact).
+
+== Installation ==
+
+1. Activate the plugin
+2. A new "Set reply as private" checkbox will be added to the new reply creation form
+3. Replies' private status can be changed by editing the reply
+
+== Screenshots ==
+
+1. Screenshot 1
+2. Screenshot 2
+3. Screenshot 3
+4. Screenshot 4
+
+
+== Changelog ==
+
+= 1.0.11 =
+
+* Fixed a bug that made private replies open to logged-out users
+
+= 1.0.10 =
+
+* Fixed a bug that caused authorized users to not be able to see private replies
+
+= 1.0.9 =
+
+* Fixed a bug with topic authors would receive email notifications on private replies by other authors on the same thread
+
+= 1.0.8 =
+
+* Fixed a bug with topic authors could see private replies of other authors in the same thread
+
+= 1.0.7 =
+
+* Fixed a compatibility problem with GD bbPress Attachments that resulted in attached files not being private.
+
+= 1.0.6 =
+
+* Fixed missing argument error.
+
+= 1.0.5 =
+
+* Fixed a bug with private replies showing up in search results
+
+= 1.0.4 =
+
+* Fixed another bug with new reply notifications including private details.
+
+= 1.0.3 =
+
+* Fixed a bug with new reply notifications including private details.
+
+= 1.0.2 =
+
+* Darkened background of private replies
+
+= 1.0.1 =
+
+* Fixed a bug with private replies in user's profiles.
+
+= 1.0 =
+
+* Fixed a bug that caused topic subscribers to get emails with new private replies that they didn't have permission to read
+
+= 0.2 =
+
+* Fixed a bug that prevented topic authors from seeing private admin replies
+* Changes the capability required to see private replies from bbp_forums_admin to publish_forums
+* Changed private reply backgrounds to blue to help distinguish them from sticky topics
+* Improved French translation
+
+= 0.1 =
+
+* First beta release!
